aad5c0d1d82e85a26d737c9bde89b254740d9c45
tjtkdvl <tjtkdvl@gmail.com>
2024-07-31 13:39:30 +0900

    Initial commit

---
+++ src/test/java/com/example/couplemaker/CouplemakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.couplemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouplemakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
